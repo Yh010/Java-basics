026cdbf9f2cfb2965ccdf8db362653f7a577ba34
Yash Hegde <yash4success@gmail.com>
2025-09-21 17:32:47 +0530

    Spring managing a Bean

--- src/main/java/com/yash/learning/HelloWorldConfiguration.java
+++ src/main/java/com/yash/learning/HelloWorldConfiguration.java
@@ -1,9 +1,14 @@
 package com.yash.learning;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //config for spring
 @Configuration
 public class HelloWorldConfiguration {
 
+    @Bean
+    public String name() {
+        return "Yash";
+    }
 }
