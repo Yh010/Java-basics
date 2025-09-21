c41b054f0ca0dec4a10aa7c977d060770403f250
Yash Hegde <yash4success@gmail.com>
2025-09-21 16:58:22 +0530

    starting spring

--- src/main/java/com/yash/learning/App01GamingBasicJava.java
+++ src/main/java/com/yash/learning/App01GamingBasicJava.java
@@ -5,7 +5,7 @@ import com.yash.learning.game.PacManGame;
 //import com.yash.learning.game.MarioGame;
 //import com.yash.learning.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
     public static void main(String[] args) {
         // var game = new SuperContraGame();
         var game = new PacManGame();
---
+++ src/main/java/com/yash/learning/App02HelloWorldSpring.java
@@ -0,0 +1,9 @@
+package com.yash.learning;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+    }
+}
