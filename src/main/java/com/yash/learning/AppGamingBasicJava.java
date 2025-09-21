7630db417ef1d0c59fe3a8003eca845c50429855
Yash Hegde <yash4success@gmail.com>
2025-09-21 16:02:47 +0530

    pacman game

--- src/main/java/com/yash/learning/AppGamingBasicJava.java
+++ src/main/java/com/yash/learning/AppGamingBasicJava.java
@@ -1,12 +1,14 @@
 package com.yash.learning;
 
 import com.yash.learning.game.GameRunner;
-import com.yash.learning.game.MarioGame;
-import com.yash.learning.game.SuperContraGame;
+import com.yash.learning.game.PacManGame;
+//import com.yash.learning.game.MarioGame;
+//import com.yash.learning.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        var game = new SuperContraGame();
+        // var game = new SuperContraGame();
+        var game = new PacManGame();
         var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
