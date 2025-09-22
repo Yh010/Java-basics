1140224f2deee5ff323085109998f73fb2a42e2a
Yash Hegde <yash4success@gmail.com>
2025-09-22 07:58:50 +0530

    records and custom object

--- src/main/java/com/yash/learning/App02HelloWorldSpring.java
+++ src/main/java/com/yash/learning/App02HelloWorldSpring.java
@@ -6,5 +6,6 @@ public class App02HelloWorldSpring {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
     }
 }
--- src/main/java/com/yash/learning/HelloWorldConfiguration.java
+++ src/main/java/com/yash/learning/HelloWorldConfiguration.java
@@ -3,12 +3,26 @@ package com.yash.learning;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//config for spring
+record Person(String name, int age) {
+};
+
+// config for spring
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name() {
         return "Yash";
+    }
+
+    @Bean
+    public int age() {
+        return 10;
+    }
+
+    @Bean
+    public Person person() {
+        var person = new Person("rajesh", 10);
+        return person;
     }
 }
